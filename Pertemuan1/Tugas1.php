26057c4731fb075cf649133d5ece4b2941f77782
IchtiarRomadhoni <132804935+IchtiarRomadhoni@users.noreply.github.com>
2024-03-09 00:24:31 +0700

    Add files via upload

---
+++ Pertemuan1/Tugas1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan Pertama</title>
+    <style>
+    h1 {
+        color: blue;
+        text-align: center;
+    }
+
+    table {
+        width: 300px;
+        margin: 0 auto;
+        color:green;
+        font-size: 1.5em;
+    }
+
+    .nama {
+        font-weight: bold;
+    }
+    </style>
+</head>
+
+<body>
+    <?php echo "
+   <h1>BIODATA</h1> 
+   <table cellpadding='5' cellspacing='0' border='0'>
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td class='nama'>M. Ichtiar Romadhoni</td>
+    </tr>
+    <tr>
+        <td>Email</td>
+        <td>:</td>
+        <td>Rdichtiar@gmail.com</td>
+    </tr>
+    <tr>
+        <td>Jurusan</td>
+        <td>:</td>
+        <td>Teknik Informatika</td>
+    </tr>
+   </table>
+   " ?>
+</body>
+
+</html>
